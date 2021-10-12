856ddcd05d517580b5b4c75be513f2d8b54b4ea4
Bhagyashribarge <91658058+Bhagyashribarge@users.noreply.github.com>
2021-10-12 20:24:43 +0530

    Add files via upload

---
+++ guessgameproject.py
@@ -0,0 +1,28 @@
+import random 
+name=input("enter your name")
+print("Good luck !",name)
+words=['rainbow','computer','science','python','mathematics','water','question']
+word=random.choice(words)
+print("guess the charecters")
+guesses=''
+turns=12
+while turns>0:
+    failed=0
+    for char in word:
+        if char in guesses:
+            print(char)
+        else:
+            print("_")
+            failed +=1
+    if failed==0:
+        print("you win")
+        print("the word is",word)
+        break
+    guess=input("guess a character:")
+    guesses+=guess
+    if guess not in word:
+        turns-=1
+        print("wrong")
+        print("you have",+turns,"more guesses")
+        if turns==0:
+            print("you loose")
